0765b9025ddbd16643615ce023ead5fda1ea6103
HaniyaAnis <55890173+HaniyaAnis@users.noreply.github.com>
2019-10-11 15:14:08 +0500

    Add files via upload

---
+++ Inverse String.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1, str2 As String
+        Dim index As Integer
+        Dim nextchar As Char
+
+        str1 = ""
+        str2 = ""
+        index = 0
+        nextchar = ""
+
+        Console.Write(" ENTER STRING: ")
+        str1 = Console.ReadLine
+
+        For index = 1 To Len(str1)
+            nextchar = Right(str1, index)
+            str2 = str2 & nextchar
+
+        Next
+
+        Console.Write("inverse string: " & str2)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
